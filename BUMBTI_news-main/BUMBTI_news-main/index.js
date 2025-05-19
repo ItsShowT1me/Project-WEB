b13930c7ac7a64036d9ac8e1edca121f2cfd4551
ItsShowT1me <167810506+ItsShowT1me@users.noreply.github.com>
2025-05-19 19:19:13 +0700

    Delete BUMBTI_news-main/BUMBTI_news-main directory

--- BUMBTI_news-main/BUMBTI_news-main/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('languageSelect').addEventListener('change', function() {
-    alert('คุณเลือกภาษา: ' + this.options[this.selectedIndex].text);
-});
-document.getElementById('languageLevel').addEventListener('change', function() {
-    alert('คุณเลือก: ' + this.value);
-});
