1973877a2ff54e0eb920876420ad5c011d371329
ItsShowT1me <167810506+ItsShowT1me@users.noreply.github.com>
2025-07-02 23:52:32 +0700

    Add files via upload

---
+++ PHP/delete.php
@@ -0,0 +1,11 @@
+//<?php
+//include 'db.php';
+//
+//$sql = "DELETE FROM users WHERE id=1";
+//
+//if ($conn->query($sql) === TRUE) {
+//    echo "ลบข้อมูลแล้ว";
+//} else {
+//    echo "ลบไม่ได้: " . $conn->error;
+//}
+//?>
---
+++ PHP/insert.php
@@ -0,0 +1,14 @@
+//<?php
+//include 'db.php';
+//
+//$name = "Narakorn";
+//$email = "me@example.com";
+//
+//$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+//
+//if ($conn->query($sql) === TRUE) {
+//    echo "เพิ่มข้อมูลสำเร็จ";
+//} else {
+//    echo "เกิดข้อผิดพลาด: " . $conn->error;
+//}
+//?>
