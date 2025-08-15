cc556c91ddbe3fa9b9fd8f67a9074114311706f0
ItsShowT1me <167810506+ItsShowT1me@users.noreply.github.com>
2025-08-16 04:42:16 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
+
+
 <?php
-
-
-$db_host = 'dpg-d7zb1br1leoes73cnfv1g-a'; // Hostname from Render
+$db_host = 'dpg-d7zb1br1leoes73cnfv1g-a.singapore-postgres.render.com'; // Use external hostname
 $db_user = 'login_db';                     // Username from Render
 $db_pass = 'TeJnx0NksvJ1JIPtwtij8JevLuDK6xG'; // Password from Render
 $db_name = 'login_db_ruj9';                // Database name from Render
@@ -13,4 +13,5 @@ if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }
 ?>
+
 
