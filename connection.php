ccd1ceaf301c2eb7b551a73e90539f6987a7f86a
ItsShowT1me <167810506+ItsShowT1me@users.noreply.github.com>
2025-08-16 05:32:22 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,16 +1,14 @@
 <?php
-$db_host = 'dpg-d2fpib7diees73cnfvlg-a.singapore-postgres.render.com';
+$db_host = 'dpg-d7zb1br1leoes73cnfv1g-a.singapore-postgres.render.com';
 $db_user = 'login_db';
-$db_pass = 'IoJndXlNSuvJJIJIPwttdj10vzLuDKGr';
+$db_pass = 'TeJnx0NksvJ1JIPtwtij8JevLuDK6xG';
 $db_name = 'login_db_zu0j';
 $db_port = '5432';
 
 $dsn = "pgsql:host=$db_host;port=$db_port;dbname=$db_name;sslmode=require";
 try {
-    $conn = new PDO($dsn, $db_user, $db_pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $con = new PDO($dsn, $db_user, $db_pass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
 ?>
-
-
