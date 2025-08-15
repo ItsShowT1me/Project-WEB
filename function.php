1267411886618773f6f0c38a98346ac6f3cc748e
ItsShowT1me <167810506+ItsShowT1me@users.noreply.github.com>
2025-08-16 05:37:00 +0700

    Update function.php

--- function.php
+++ function.php
@@ -1,16 +1,17 @@
 <?php
+<?php
 
 function check_login($con)
 {
     if (isset($_SESSION['user_id'])) {
         $id = $_SESSION['user_id'];
-        $query = "SELECT * FROM users WHERE user_id = '$id' LIMIT 1";
+        $query = "SELECT * FROM users WHERE user_id = :id LIMIT 1";
 
-        $result = mysqli_query($con, $query);
+        $stmt = $con->prepare($query);
+        $stmt->execute(['id' => $id]);
+        $user_data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($result && mysqli_num_rows($result) > 0) {
-
-            $user_data = mysqli_fetch_assoc($result);
+        if ($user_data) {
             return $user_data;
         }
     }
